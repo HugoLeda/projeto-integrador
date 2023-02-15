01d4f0adecfb6de43a5558c291e5ba1604d130aa
João Hugo Leda <74215171+HugoLeda@users.noreply.github.com>
2023-02-14 22:10:13 -0300

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,11 +1,15 @@
 # Fazer um algoritmo que leia um número inteiro e escreva o seu antecessor e o seu sucessor
 
-def printAntecessorSucessor(numero):
-    antecessor = numero - 1
-    sucessor = numero + 1    
-    print(f"O antecessor é: {antecessor}\nO sucessor é: {sucessor}")
-    
-print("Mostar antecessor e sucessor\n")
-numero = int(input("Digite um número: "))
+i = 0
+while (i != 1):
+    def printAntecessorSucessor(numero):
+        antecessor = numero - 1
+        sucessor = numero + 1    
+        print(f"O antecessor é: {antecessor}\nO sucessor é: {sucessor}")
+        
+    print("Mostar antecessor e sucessor\n")
+    numero = int(input("Digite um número: "))
 
-printAntecessorSucessor(numero)
+    printAntecessorSucessor(numero)    
+
+    i = int(input("Digite 1 para sair: "))
